3c58c41486530417afc819132e42b79dff7c2f92
Rafael Corsi <corsiferrao@gmail.com>
2016-06-07 18:07:05 -0300

    Commit

--- 15-DAC/LCD-Desenhos/Debug/src/ili93xx_example.d
+++ 15-DAC/LCD-Desenhos/Debug/src/ili93xx_example.d
@@ -133,7 +133,8 @@ src/ili93xx_example.d src/ili93xx_example.o: ../src/ili93xx_example.c \
  ../src/ASF/common/services/serial/sam_uart/uart_serial.h \
  ../src/ASF/sam/drivers/uart/uart.h ../src/ASF/sam/drivers/usart/usart.h \
  ../src/config/conf_uart_serial.h ../src/ASF/sam/drivers/tc/tc.h \
- ../src/ASF/sam/drivers/pio/pio_handler.h ../src/img.h
+ ../src/ASF/sam/drivers/pio/pio_handler.h ../src/img.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\math.h
 
 ../src/asf.h:
 
@@ -422,3 +423,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-no
 ../src/ASF/sam/drivers/pio/pio_handler.h:
 
 ../src/img.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\math.h:
